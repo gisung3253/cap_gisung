c101f76ec4c31d9fc0bf51c7dd1c8f9896fec731
all_might_00 <144012404+gisung3253@users.noreply.github.com>
2025-04-08 13:26:09 +0900

    Update TimetableDataSet.jsx

--- client/src/Timetable/TimetableDataSet.jsx
+++ client/src/Timetable/TimetableDataSet.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-import "./timetable.css";
+import "./Timetable.css";
 import Layout from "../Layout/Layout";
 
 import { useNavigate } from "react-router-dom";
